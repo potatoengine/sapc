2b34ea44d184ba5b26ad8c5ab035b06ca38966de
Sean Middleditch <sean.middleditch@gmail.com>
2020-09-08 19:44:49 -0700

    Remove old parser

--- source/main.cc
+++ source/main.cc
@@ -2,7 +2,6 @@
 // This is free and unencumbered software released into the public domain.
 // See LICENSE.md for more details.
 
-#include "state.hh"
 #include "parser.hh"
 #include "ast.hh"
 #include "string_util.hh"
