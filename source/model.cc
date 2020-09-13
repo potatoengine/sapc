a0789465c5508c3e09eae9365ebb690381e5ce2c
Sean Middleditch <sean.middleditch@gmail.com>
2020-09-13 14:36:46 -0700

    Encode locations in types listed in the json

--- source/model.cc
+++ source/model.cc
@@ -57,6 +57,16 @@ namespace sapc {
             return attrs_json;
         };
 
+        auto loc_to_json = [&](Location const& loc) {
+            auto loc_json = json::object();
+            loc_json["filename"] = loc.filename.string();
+            if (loc.line > 0)
+                loc_json["line"] = loc.line;
+            if (loc.column > 0)
+                loc_json["column"] = loc.column;
+            return loc_json;
+        };
+
         doc["attributes"] = attrs_to_json(module.attributes);
 
         auto modules_json = json::array();
@@ -77,6 +87,7 @@ namespace sapc {
                 type_json["base"] = type.base;
             if (!type.attributes.empty())
                 type_json["attributes"] = attrs_to_json(type.attributes);
+            type_json["location"] = loc_to_json(type.location);
 
             if (type.category == Type::Category::Enum) {
                 auto values = json::array();
